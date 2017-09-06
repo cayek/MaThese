4259a8ddb987daf98d87cacfaa77570b9c982930
cayek <kevin.caye@gmail.com>
2017-09-06 09:47:20 +0200

    tess3

---
+++ Rpackage/R/tess3-method.R
@@ -0,0 +1,67 @@
+#' Sampler class
+#'
+#'
+#' @export
+Method <- function(name = "A method", hypothesis.testing.method = NULL,
+                   impute.genotype.method = imputeByMean(),
+                   nickname = NULL) {
+  structure(list(name = name,
+                 hypothesis.testing.method = hypothesis.testing.method,
+                 impute.genotype.method = impute.genotype.method,
+                 nickname = nickname), class = "Method")
+}
+
+
+
+################################################################################
+# Methods
+
+#' fit
+#'
+#' @export
+fit <- function(m, dat, reuse = FALSE, ...){
+  UseMethod("fit")
+}
+
+#' name
+#'
+#' @export
+name <- function(m){
+  UseMethod("name")
+}
+
+#' Run the association scan method
+#'
+#' @export
+run <- function(m, dat, ...){
+  UseMethod("run")
+}
+
+#' Clean all estimates
+#'
+#' @export
+clean <- function(m) {
+  UseMethod("clean")
+}
+
+#' Title
+#'
+#'
+#' @export
+print.Method <- function(x) {
+  str(x)
+}
+
+
+#' @export
+name.Method <- function(m) {
+
+  if (!is.null(m$nickname)) {
+    return(m$nickname)
+  }
+  if (!is.null(m$name)) {
+    paste0(m$name, "|",name(m$hypothesis.testing.method))
+  } else {
+    "A method"
+  }
+}
